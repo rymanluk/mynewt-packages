c7ec3f2578812d1a7e9ce77a8bc5c6e94fa42204
Stephane D'Alu <sdalu@sdalu.com>
2018-04-06 17:22:08 +0200

    using syscfg

--- hw/display/lvgl/include/lv_conf.h
+++ hw/display/lvgl/include/lv_conf.h
@@ -28,9 +28,9 @@
  *===================*/
 
 /* Horizontal and vertical resolution of the library.*/
-#define LV_HOR_RES          (320)
-#define LV_VER_RES          (240)
-#define LV_DPI              100
+#define LV_HOR_RES          MYNEWT_VAL(LV_HORIZONTAL_RESOLUTION)
+#define LV_VER_RES          MYNEWT_VAL(LV_VERTICAL_RESOLUTION)
+#define LV_DPI              MYNEWT_VAL(LV_DPI)
 
 /* Size of VDB (Virtual Display Buffer: the internal graphics buffer).
  * Required for buffered drawing, opacity and anti-aliasing
